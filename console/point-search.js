6753a5315a7bf3f1d86c5e2e9192be4ba2012d77
erelsgl <erelsgl@gmail.com>
2014-03-05 18:03:38 +0200

    point search

--- console/point-search.js
+++ console/point-search.js
@@ -20,11 +20,12 @@ var initial_points = [
   {x:140, y:Y_RANGE},
   {x:180, y:Y_RANGE},
   {x:200, y:Y_RANGE},
-  {x:220, y:Y_RANGE},
-  {x:260, y:Y_RANGE},
+  {x:210, y:Y_RANGE},
+  {x:230, y:Y_RANGE},
+  {x:270, y:Y_RANGE},
 
-  {x:200,y:300},
-  
+  {x:205,y:0},
+
   {x:231, y:Y_RANGE-20},
   {x:171, y:Y_RANGE-21},
   ];
@@ -39,22 +40,25 @@ var start=new Date();
 var fs = require('fs');
 var results = "";
 var step = 2;
+var minDisjointSetSize = Infinity;
 for (var x=100; x<=300; x+=step) {
 	for (var y=0; y<=200; y+=step) {
 		var points = initial_points.slice(0);
 //		points[5].x = x;
 //		points[5].y = Y_RANGE-y;
-		points[6].x = x;
-		points[6].y = Y_RANGE-y;
-		points[7].x = X_RANGE-x;
+		points[7].x = x;
 		points[7].y = Y_RANGE-y;
+		points[8].x = X_RANGE-x;
+		points[8].y = Y_RANGE-y;
 		makeXYUnique(points, xminWall, xmaxWall, yminWall, ymaxWall);
 		var disjointset = maximumDisjointSet(squaresTouchingPoints(points, xminWall, xmaxWall, yminWall, ymaxWall));
 		results += (x+"\t"+y+"\t"+disjointset.length+"\n");
+		if (disjointset.length<minDisjointSetSize)
+			minDisjointSetSize = disjointset.length;
 	}
 }
 fs.writeFile("results.dat", results);
 
 var elapsed=new Date()-start;
-console.log("time: "+elapsed+" [ms]");
+console.log("minDisjointSetSize="+minDisjointSetSize+" time: "+elapsed+" [ms]");
 
